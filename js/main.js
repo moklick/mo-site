3f3e0419d9ebb14182c8dcce5d7e681937d25cba
moklick <info@moritzklack.com>
2014-08-08 03:11:08 +0200

    big update

--- js/main.js
+++ js/main.js
@@ -1,28 +1,53 @@
-$(document).ready(function(){
-	var url = "https://api.twitter.com/1/statuses/user_timeline.json";
-	$.ajax({
-			url : url,
-			type : "POST",
-			data : {
-				screen_name : 'moklick',
-				include_rts : true,
-				count : 1,
-				include_entities : true
-			},
-			dataType : "jsonp",
-			success : function(data){
-				console.log(data[0]);
-				if(data){
-					var text = replaceTextWithLink(data[0].text)
-					$('#last-tweet .module-text').html(text);	
-				}else{
-					$('#last-tweet').hide();
-				}
-			}
-	});
-});
+ var $contactForm = $('#contact-form'),
+    $sendBtn = $contactForm.find('button'),
+    $success = $('.success'),
+    $error = $('.error');
 
-function replaceTextWithLink(text) {
-    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text.replace(exp,"<a class='sub-link' target='_blank' href='$1'>$1</a>");
-}
+ $contactForm .find('input,textarea').on('focus', function(){
+    $(this).parents('.input-wrapper').find('.error').removeClass('active');
+ });
+
+ $contactForm.on('submit', function(e) {
+     e.preventDefault();
+
+     $sendBtn.addClass('is-loading');
+     $error.removeClass('active');
+     $success.removeClass('active');
+
+     $.ajax({
+         type: "POST",
+         url: $(this).attr('action'),
+         data: {
+             name: 'moritzklack.com',
+             email: $('.email-field').val(),
+             subject: 'moritzklack.com',
+             message: $contactForm.find('textarea').val()
+         },
+         success: function(data) {
+             $sendBtn.removeClass('is-loading');
+             var data = JSON.parse(data);
+
+            $error.removeClass('active');
+            $success.removeClass('active');
+
+             if (data.invalid) {
+                 if (data.invalid.email) {
+                     $('.email-error').text(data.invalid.email).addClass('active');
+                 }
+                 if (data.invalid.message) {
+                     $('.message-error').text(data.invalid.message).addClass('active');
+                 }
+             } else {
+                 $contactForm.find('input, textarea').val('');
+                 $success.addClass('active');
+                 setTimeout(function(){
+                    $success.removeClass('active');
+                 },3000);
+             }
+
+         },
+         error: function() {
+             $sendBtn.removeClass('is-loading');
+         }
+     });
+ });
